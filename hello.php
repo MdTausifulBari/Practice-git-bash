f1f0919ae7665541fb00ebd6c2e731ad76948b06
mdtausifulbari <tausiful.bari@northsouth.edu>
2023-12-04 16:05:30 +0600

    Making my first commit

---
+++ hello.php
@@ -0,0 +1,26 @@
+<?php
+
+// Terminal php hello.php
+// Terminal php -S 127.0.0.1:8000 hello.php
+// PHP server
+
+// echo "Hello World! I want to become a PHP developer.";
+
+$a = 10;
+$b = 20;
+$c = 20;
+$d = 40;
+$e = 50;
+$f = 60;
+$g = 70;
+
+$sum = $a + $b + $c + $d + $e + $f + $g;
+
+echo $sum;
+
+
+// Bug finding
+// Process Understand
+// Business Logic
+// Code Flow
+// Step By Step Execution
